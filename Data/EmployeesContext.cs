7ec78f192fe93451233a4d64460a4611c06242da
sujathadattala <sujatha.dattala@gmail.com>
2024-07-22 19:09:17 +0530

    Add files via upload
    api project

---
+++ Data/EmployeesContext.cs
@@ -0,0 +1,14 @@
+﻿using ContactWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactWebApi.Data
+{
+    public class EmployeesContext : DbContext
+    {
+        public EmployeesContext(DbContextOptions<EmployeesContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
---
+++ Migrations/20240719085044_EmployeeContextDb.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactWebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmployeeContextDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Firstname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Lastname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmailId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
